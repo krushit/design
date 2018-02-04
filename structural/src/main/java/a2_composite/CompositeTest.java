2b66684c28a72c9bbec96805f5a68e4026cd076b
Kshitij Agarwal <kagarwal@roku.com>
2018-02-04 15:37:07 -0800

    composite design pattern

---
+++ structural/src/main/java/a2_composite/CompositeTest.java
@@ -0,0 +1,35 @@
+package a2_composite;
+
+/**
+ * Created by kagarwal on 2/4/18.
+ */
+public class CompositeTest {
+    public static void main(String[] args) {
+        Component hardDrive = new Leaf(4000, "HDD");
+        Component mouse = new Leaf(500, "Mouse");
+        Component monitor = new Leaf(8000, "Monitor");
+        Component ram = new Leaf(3000, "RAM");
+        Component cpu = new Leaf(9000, "CPU");
+
+        Composite peripheral = new Composite("Peripherals");
+        Composite cabinet = new Composite("Cabinet");
+        Composite motherboard = new Composite("Motherboard");
+        Composite computer = new Composite("Computer");
+
+        peripheral.addComponent(mouse);
+        peripheral.addComponent(monitor);
+
+        motherboard.addComponent(cpu);
+        motherboard.addComponent(ram);
+
+        cabinet.addComponent(hardDrive);
+        cabinet.addComponent(motherboard);
+
+        computer.addComponent(peripheral);
+        computer.addComponent(cabinet);
+
+        //peripheral.showPrice();
+        computer.showPrice();
+
+    }
+}
---
+++ structural/src/main/java/a2_composite/ComputerPart.java
@@ -0,0 +1,48 @@
+package a2_composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kagarwal on 2/4/18.
+ */
+
+interface Component {
+    void showPrice();
+}
+
+class Leaf implements Component{
+    int price;
+    String name;
+
+    public Leaf(int price, String name) {
+        this.price = price;
+        this.name = name;
+    }
+
+    public void showPrice() {
+        System.out.println("Name: " + name + ", Price: " + price);
+    }
+}
+
+class Composite implements Component{
+
+    String name;
+    List<Component> components = new ArrayList<Component>();
+
+    public Composite(String name) {
+        this.name = name;
+    }
+
+    public void addComponent(Component comp) {
+        components.add(comp);
+    }
+
+    public void showPrice() {
+        System.out.println("Component Name: " + name);
+        for (Component c: components) {
+            c.showPrice();
+        }
+        System.out.println();
+    }
+}
